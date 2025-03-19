9f3af1d9c3420bf164ff6633980a6d08dda28569
Daniil Komandirov <168469965+cmder137@users.noreply.github.com>
2025-03-19 22:44:52 +0300

    Update ComplexNumber.cs

--- ComplexNumber.cs
+++ ComplexNumber.cs
@@ -1,4 +1,4 @@
-﻿public struct ComplexNumber
+public struct ComplexNumber
 {
     public double x;
     public double y;
@@ -25,6 +25,16 @@
 
         temp.x = a.x * b.x - a.y * b.y;
         temp.y = a.x * b.y + a.y * b.x;
+
+        return temp;
+    }
+
+    public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+    {
+        ComplexNumber temp = new ComplexNumber(0, 0);
+
+        temp.x = (a.x * b.x + a.y * b.y) / b.SquareOfAbs();
+        temp.y = (a.y * b.x - a.x * b.y) / b.SquareOfAbs();
 
         return temp;
     }
